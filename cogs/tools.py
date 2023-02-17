d5674b028d2e94e825a68ff20d39e11c0bb3ddfa
root <root@toonisland.online>
2023-02-17 01:25:36 +0000

    fix adfly by seperating cmds

--- cogs/tools.py
+++ cogs/tools.py
@@ -11,88 +11,143 @@ from base64 import b64decode
 from discord.ext import commands
 from urllib.request import urlopen, Request
 
+
 class Tools(commands.Cog):
 
     """Cog for Tools commands"""
 
     def __init__(self, bot):
         self.bot = bot
-        
-    #Adfly Command
-    @commands.command(pass_context=True)
-    async def adfly(self, ctx, search):
+
+    # Adfly Command
+    @commands.command(
+        pass_context=True, aliases=["adfly_2014", "adfly_2015", "adfly_2016"]
+    )
+    async def adfly_old(self, ctx, search):
         if "http" not in search:
             search = "https://" + search
-        req = Request(search, headers={'User-Agent': 'Chrome/91.0.4472.77'})
+        req = Request(search, headers={"User-Agent": "Chrome/91.0.4472.77"})
         data_ = urlopen(req)
         data = data_.read()
         ysmm = data.split(b"ysmm = '")[1].split(b"';")[0]
-        decrypted_url = ysmm.decode('utf-8') # Decrypt the URL
+        decrypted_url = ysmm.decode("utf-8")  # Decrypt the URL
+        zeros, ones = "", ""
 
-        zeros, ones = '', ''
         for num, letter in enumerate(decrypted_url):
-            if num % 2 == 0: zeros += decrypted_url[num]
-            else: ones = decrypted_url[num] + ones
+            if num % 2 == 0:
+                zeros += decrypted_url[num]
+            else:
+                ones = decrypted_url[num] + ones
 
-        key = list(zeros + ones)
-        i=0
-        while i != len(key):
-            hlp=0
-            if str(key[i]).isnumeric():
-                for y in range(i+1,len(key)):
-                    if str(key[y]).isnumeric():
-                        hlp=hlp+1
-                        temp=int(key[i])^int(key[y])
-                        if int(temp) < 10:
-                            key[i]=str(temp)
-                        i=y+1
-                        break
-            if hlp==0:
-                i=i+1
-        temp="".join(key)
-        key=temp
+        key = zeros + ones
         key = b64decode(key.encode("utf-8"))
 
-        decrypted_url=key[16:-16]
+        decrypted_url = key[2:].decode("utf-8")
         decrypted_url = urllib.parse.unquote(decrypted_url)
         try:
-            m = re.search ( r'&dest=(.*)', decrypted_url)
+            m = re.search(r"&dest=(.*)", decrypted_url)
             decrypted_url = m.group(1)
         except:
             pass
+        decrypted_url = decrypted_url.replace(" ", "%20")
         embed = discord.Embed(
             title=f"Adf.ly Decoder",
             description=decrypted_url,
             colour=0x98FB98,
-            timestamp=ctx.message.created_at)
+            timestamp=ctx.message.created_at,
+        )
         try:
             embed.set_thumbnail(url="https://i.ibb.co/qJ0ZxTD/646023.png")
         except:
             pass
-        embed.set_footer(text=f"Ran by: {ctx.message.author} • Yours truly, {self.bot.user.name}")
+        embed.set_footer(
+            text=f"Ran by: {ctx.message.author} • Yours truly, {self.bot.user.name}"
+        )
         embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar.url)
         await ctx.send(content=None, embed=embed)
 
-    #Technic Command
+    # Adfly Command
+    @commands.command(pass_context=True)
+    async def adfly(self, ctx, search):
+        if "http" not in search:
+            search = "https://" + search
+        req = Request(search, headers={"User-Agent": "Chrome/91.0.4472.77"})
+        data_ = urlopen(req)
+        data = data_.read()
+        ysmm = data.split(b"ysmm = '")[1].split(b"';")[0]
+        decrypted_url = ysmm.decode("utf-8")  # Decrypt the URL
+
+        zeros, ones = "", ""
+        for num, letter in enumerate(decrypted_url):
+            if num % 2 == 0:
+                zeros += decrypted_url[num]
+            else:
+                ones = decrypted_url[num] + ones
+
+        key = list(zeros + ones)
+        i = 0
+        while i != len(key):
+            hlp = 0
+            if str(key[i]).isnumeric():
+                for y in range(i + 1, len(key)):
+                    if str(key[y]).isnumeric():
+                        hlp = hlp + 1
+                        temp = int(key[i]) ^ int(key[y])
+                        if int(temp) < 10:
+                            key[i] = str(temp)
+                        i = y + 1
+                        break
+            if hlp == 0:
+                i = i + 1
+        temp = "".join(key)
+        key = temp
+        key = b64decode(key.encode("utf-8"))
+
+        decrypted_url = key[16:-16].decode("utf-8")
+        decrypted_url = urllib.parse.unquote(decrypted_url)
+        try:
+            m = re.search(r"&dest=(.*)", decrypted_url)
+            decrypted_url = m.group(1)
+        except:
+            pass
+        decrypted_url = decrypted_url.replace(" ", "%20")
+        embed = discord.Embed(
+            title=f"Adf.ly Decoder",
+            description=decrypted_url,
+            colour=0x98FB98,
+            timestamp=ctx.message.created_at,
+        )
+        try:
+            embed.set_thumbnail(url="https://i.ibb.co/qJ0ZxTD/646023.png")
+        except:
+            pass
+        embed.set_footer(
+            text=f"Ran by: {ctx.message.author} • Yours truly, {self.bot.user.name}"
+        )
+        embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar.url)
+        await ctx.send(content=None, embed=embed)
+
+    # Technic Command
     @commands.command(pass_context=True)
     async def technic(self, ctx, search):
         url = "https://api.technicpack.net/search?q=" + search
         params = {
-        'build': 'build',
+            "build": "build",
         }
         responce = requests.get(url, params=params)
         json = responce.json()
         packList = []
         embed = discord.Embed(
             title=f"Technic Slug Lookup",
-            description='\uFEFF',
+            description="\uFEFF",
             colour=0x98FB98,
-            timestamp=ctx.message.created_at)
+            timestamp=ctx.message.created_at,
+        )
         try:
             for pack in json["modpacks"]:
                 url2 = "https://api.technicpack.net/modpack/" + pack["slug"]
                 params2 = {
-                'build': 'build',
+                    "build": "build",
                 }
                 responce = requests.get(url2, params=params)
                 json2 = responce.json()
@@ -105,12 +160,17 @@ class Tools(commands.Cog):
         except:
             pass
         try:
-            embed.set_thumbnail(url="https://i.ibb.co/qgXtt0Z/309359763-128793633250428-4428571622506032512-n.png")
+            embed.set_thumbnail(
+                url="https://i.ibb.co/qgXtt0Z/309359763-128793633250428-4428571622506032512-n.png"
+            )
         except:
             pass
-        embed.set_footer(text=f"Ran by: {ctx.message.author} • Yours truly, {self.bot.user.name}")
+        embed.set_footer(
+            text=f"Ran by: {ctx.message.author} • Yours truly, {self.bot.user.name}"
+        )
         embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar.url)
         await ctx.send(content=None, embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Tools(bot))
