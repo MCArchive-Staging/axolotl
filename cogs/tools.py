abc4a34943a2a230ab10c2c22bb85da1e010b003
Nayla Hanegan <NaylaHanegan@gmail.com>
2024-11-10 03:57:09 -0500

    dr

--- cogs/tools.py
+++ cogs/tools.py
@@ -24,6 +24,8 @@ class Tools(commands.Cog):
 
         # Set up Chrome options
         chrome_options = Options()
+        chrome_options.binary_location = '/snap/bin/chromium'
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
 
